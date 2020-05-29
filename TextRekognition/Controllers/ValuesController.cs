8f8a293b10768bcce3e7d20907346de135ad212e
Manikanta Gudipudi <mgudipudi@factset.com>
2020-05-29 09:06:30 +0530

    First checkin

---
+++ TextRekognition/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Amazon.Textract.Internal;
+using Amazon.Textract;
+using Amazon.Textract.Model;
+using S3Object = Amazon.Textract.Model.S3Object;
+using System.IO;
+
+namespace TextRekognition.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post()
+        {
+
+            AmazonTextractClient objTextExtract = new AmazonTextractClient("AKIA23XZGIB6BEUBA5HM", "ePmW8ZsldWdQP3hmillAOMLu+A5b4ePgeBV3+/O5", Amazon.RegionEndpoint.USEast1);
+            DetectDocumentTextRequest request = new DetectDocumentTextRequest();
+            byte[] fileData = null;
+
+            using (var ms = new MemoryStream())
+            {
+                HttpContext.Request.Form.Files[0].CopyTo(ms);
+                fileData = ms.ToArray();
+                string s = Convert.ToBase64String(fileData);
+                // act on the Base64 data
+            }
+            request.Document = new Document
+            {
+                Bytes = new MemoryStream(fileData)
+            };
+
+            var task = UploadImageForRekognition(objTextExtract, request);
+            task.Wait();
+            var result = task.Result;
+            return Ok(result);
+
+        }
+
+        private async Task<DetectDocumentTextResponse> UploadImageForRekognition(AmazonTextractClient objRekClient, DetectDocumentTextRequest objDetectText)
+        {
+            return await objRekClient.DetectDocumentTextAsync(objDetectText);
+        }
+    }
+}
---
+++ TextRekognition/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace TextRekognition
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        readonly string MyPolicy = "MyPolicy";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyPolicy,
+                builder =>
+                {
+                    //builder.WithOrigins("http://localhost:5001",
+                    //                    "http://localhost:5003")
+                    builder.AllowAnyOrigin()
+                                        .AllowAnyHeader()
+                                        .AllowAnyMethod();
+                });
+                options.AddPolicy("AllowSubdomain",
+                    builder =>
+                    {
+                        builder.SetIsOriginAllowedToAllowWildcardSubdomains();
+                    });
+            });
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("MyPolicy"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseCors(MyPolicy);
+        }
+    }
+}
